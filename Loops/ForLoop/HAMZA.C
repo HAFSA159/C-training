ab1d21e2ce176e7b35e7aba3dc03285d9ff219d5
Hamza Meski <meskihamza5@gmail.com>
2023-09-14 13:30:08 +0200

    Up

---
+++ Loops/ForLoop/HAMZA.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+        
+        for (int i = 0; i < 10; i++) {
+            printf("R\n");
+        }
+
+        printf("EOP\n");
+
+        return 0;
+}
