26541579a6914e3f32d59b7cf9a9fc217da364cd
root <root@LAPTOP-E3F8CVE6.localdomain>
2023-09-14 10:02:58 +0100

    condition

---
+++ Conditions/mila.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age ;
+	printf ("enter your age : \n");
+	scanf("%d", &age);
+	;
+    if (age >= 18)
+    {
+       printf ("Vous etes majeur !\n");
+    }
+    else
+	printf ("Vous etes mineur !\n");
+
+
+    return 0;
+}
