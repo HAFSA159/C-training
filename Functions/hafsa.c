bba9ff2ea8773e2e0950f098f2a8f963d740490a
root <root@LAPTOP-E3F8CVE6.localdomain>
2023-09-15 16:37:08 +0100

    min et max

---
+++ Functions/hafsa.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+float Max (float a, float b){
+
+float max;
+
+if (a>b)
+        max=a;
+else
+        max=b;
+return max;
+}
+
+
+
+float Min (float a, float b){
+
+float min;
+
+if (a<b)
+
+        min=a;
+else
+
+        min=b;
+return min;
+}
