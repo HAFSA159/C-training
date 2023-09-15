79d225d7c047c62ef84abdeae8b7a4b704a05168
kholoud001 <kholoud.sanak@gmail.com>
2023-09-15 10:05:18 +0100

    dowhile

---
+++ Loops/do-while/kholoud.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+int i,n;
+
+printf("enter how many itteration you want \n");
+scanf("%d",&n);
+ 
+i=1;
+do {
+
+printf("itteration number %d \n",i);
+i++;
+
+
+}while(i<=n);
+
+
+return(0);
+}
