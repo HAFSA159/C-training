60322cce70c05fc140640b14c582fe5f010e579e
root <root@LAPTOP-E3F8CVE6.localdomain>
2023-09-16 10:02:42 +0100

    array

---
+++ Tabs/hafsa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main() {
+
+ float la_note[5];
+ float notes;
+ float somme=0 , moyenne;
+
+ for (int i=0;i<5;i++){
+
+ printf("Enter les notes\n");
+ scanf("%f", &notes);
+
+ la_note[i]=notes;
+ somme = somme + la_note[i];
+}
+ moyenne = somme / 5;
+ printf("la moyenne de la classe c'est: %.2f \n", moyenne);
+}
