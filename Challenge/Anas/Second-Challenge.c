79826e8d8f7e99b6ebd2396953395ad97f9e92c7
AnasClod <anas500.diabi@gmail.com>
2023-09-19 16:01:18 +0100

    sd

--- Challenge/Anas/Second-Challenge.c
+++ Challenge/Anas/Second-Challenge.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 	typedef struct {
-	int cor1,cor2;
+	float cor1,cor2;
 	} cord;
 
 	int main(){
 
 	cord p1,p2;
 	printf("Entrez les coordonnées du point 1 (x1 y1) :");
-	scanf("%d",&p1.cor1);
-	scanf("%d",&p1.cor2);
+	scanf("%f %f",&p1.cor1,&p1.cor2);
+
 	printf("Entrez les coordonnées du point 2 (x2 y2) :");
-	scanf("%d",&p2.cor1);
-	scanf("%d",&p2.cor2);
+	scanf("%f %f",&p2.cor1,&p2.cor2);
+	
 }
