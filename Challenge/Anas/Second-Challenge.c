444ddcd9ca14411a4aaab287106c826cf080c41c
AnasClod <anas500.diabi@gmail.com>
2023-09-19 14:41:18 +0100

    jj

---
+++ Challenge/Anas/Second-Challenge.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+	typedef struct {
+	int cor1,cor2;
+	} cord;
+
+	int main(){
+
+	cord cord1;
+	printf("Entrez les coordonnées du premier point (x1 y1) :"):
+}
