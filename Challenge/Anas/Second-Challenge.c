c90e0fc072d712b45e40a04643c3524f6f3c80f5
AnasClod <anas500.diabi@gmail.com>
2023-09-19 15:20:49 +0100

    sd

--- Challenge/Anas/Second-Challenge.c
+++ Challenge/Anas/Second-Challenge.c
@@ -6,6 +6,11 @@
 
 	int main(){
 
-	cord cord1;
-	printf("Entrez les coordonnées du premier point (x1 y1) :"):
+	cord p1,p2;
+	printf("Entrez les coordonnées du point 1 (x1 y1) :");
+	scanf("%d",&p1.cor1);
+	scanf("%d",&p1.cor2);
+	printf("Entrez les coordonnées du point 2 (x2 y2) :");
+	scanf("%d",&p2.cor1);
+	scanf("%d",&p2.cor2);
 }
