f9cdbea54cb8d56c9a1480a5016c87bde182abf9
kholoud001 <kholoud.sanak@gmail.com>
2023-09-19 14:47:55 +0100

    challenge 4

---
+++ Challenge/Kholoud/liste-etudiant.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+
+typedef struct Etudiant{
+  char name[10];
+  int age;
+} Etudiant;
+
+
+
+void EnterStudent(Etudiant *e, int n){
+    int i;
+
+for (i=0;i<n;i++){
+    printf("Entrer le nom etudiant ");
+    scanf("%s",e[i].name);
+    printf("Entrer age etudiant ");
+    scanf("%d",&e[i].age);
+}
+}
+
+void DisplayStudent(Etudiant e,int n){
+
+    printf("Bienvenue dans le gestionnaire etudiant \n\n");
+    printf("La liste des etudiants \n");
+    printf("%d. Nom : %s, Age : %d \n",n,e.name,e.age);
+}
+
+
+void DisplayStudents(Etudiant *e, int n){
+    int i;
+
+    printf("Bienvenue dans le gestionnaire etudiant \n\n");
+    printf("La liste des etudiants\n");
+    for (i=0;i<n;i++){
+    printf("%d. Nom : %s . Age : %d \n",i+1,e[i].name,e[i].age);
+    }
+}
+
+
+
+
+
+int main(){
+
+Etudiant e;
+int i,n;
+printf("Enter le nombre etudiant que vous voullez ajouter:\n");
+scanf("%d",&n);
+printf("Ajouter un etudiant: \n");
+
+EnterStudent(&e,n);
+//DisplayStudent(e,n);
+DisplayStudents(&e,n);
+
+
+    return 0;
+}
